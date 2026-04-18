7d6abc6b67893cf8db9735bc6f368650b7b45dc1
okochi-mika <hiyoko_m@icloud.com>
2026-04-18 11:32:58 +0900

    最初のコミット

---
+++ setlist.js
@@ -0,0 +1,15 @@
+// setlist.js の冒頭
+const setlistData = [
+    { title: "1. all the way ", id: "Cpjvl2Q5_Zw" }, // YouTube IDを調べて入れる
+    { title: "2. それが、愛でしょう", id: "npE8RBnBsAk" },
+    { title: "3. If ~もしも願いが叶うなら~", id: "ef1JsUS7Qkk" }, 
+    { title: "4. pavement", id: "I97eBy3BaS4" },
+    { title: "5. し・か・ら・れ・て", id: "TMoOdbjcjGU" },
+    { title: "6. Remember", id: "JjWwfUbOP0w" },
+    { title: "7. この場所が好きで", id: "UlOYSURhafg" },
+    { title: "8. ~HIKARI~", id: "vBfJBE0KqPc" },
+    { title: "9. Alone", id: "iDacFs674Kc" },
+    { title: "10. 夏花火", id: "RQNoSQl2Zzo" },
+    { title: "11. 2000EXPRESS", id: "f8Nv-5wIlbg" },
+    { title: "12. Love song on the radio", id: "EHD0Tm-wji0" }
+];
